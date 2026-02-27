b6435f309245d4ca173caa222e91f6982243a129
Alies <47625063+Alies03@users.noreply.github.com>
2026-02-27 21:29:37 +0100

    Move dinosaur repo in its own folder alongside code analysis
    This helps maven and other IDEs not to get confused on which project is what

--- analysis/src/main/java/com/code/analysis/App.java
+++ analysis/src/main/java/com/code/analysis/App.java
@@ -1,21 +1,17 @@
 package com.code.analysis;
 
-import spoon.processing.AbstractProcessor;
+import spoon.Launcher;
+import spoon.reflect.declaration.CtClass;
 
 /**
  * Hello world!
  */
 public class App {
 
-    public static class MyProcessor extends AbstractProcessor {
-        @Override
-        public void process(spoon.reflect.declaration.CtElement element) {
-            // This method will be called for each element in the code being analyzed.
-            // You can add your analysis logic here.
-        }
-    }
-
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        CtClass<?> l = Launcher.parseClass("class A { void m() { System.out.println(\"yeah\");} }");
+        System.out.println(l);
     }
 }
